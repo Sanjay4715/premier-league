17006333af64d190962144cf5993f4503c85413b
Sanjay Rai <sonjrai@gmail.com>
2021-12-09 13:15:31 +0545

    rename

--- src/Pages/Results/Results.js
+++ src/Pages/Results/Results.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import moment from "moment";
 
 import axios from "axios";
 import Navigation from "../../Component/Navigation/Navigation";
@@ -12,7 +13,6 @@ const Home = () => {
         "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json"
       )
       .then((res) => {
-        console.log(res.data.rounds);
         setMatches(res.data.rounds);
       })
       .catch((error) => console.log(error));
@@ -21,7 +21,21 @@ const Home = () => {
   return (
     <div>
       <Navigation page="results" />
-      <div style={{ padding: 20 }}></div>
+      <div style={{ padding: 20 }}>
+        {matches &&
+          matches.map((match, index) => {
+            return (
+              <div key={index}>
+                <div>{match.name}</div>
+                {match.matches.map((data, index) => (
+                  <div key={index}>
+                    <div>{moment(data.date).format("MMMM Do YYYY")}</div>
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
